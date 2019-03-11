aeb13ccddc9118080c1c9b485aabf7e044c10782
Christopher Harvey <chharvey@icloud.com>
2019-03-11 16:11:30 -0400

    Axios setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import axios from "axios";
 
 class App extends React.Component {
   onSearchSubmit(field) {
-    console.log(field);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: field
+      },
+      headers: {
+        Authorization:
+          "Client-ID 405c43e3ed5b9032ae9a5c9094ef52e385688e050868a8058e186d4838e05b60"
+      }
+    });
   }
 
   render() {
